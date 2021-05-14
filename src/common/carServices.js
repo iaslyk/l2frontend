d697e3abd44319f675a0a2630961b5546a68a362
Oliver <oliversulyok@gmail.com>
2021-05-14 18:41:05 +0200

    Created files for services and generic carList.jsx

---
+++ src/common/carServices.js
@@ -0,0 +1,29 @@
+// services for carList
+
+
+const carsMakeHead = ["Car Make Name", "Car Make Abbreviation"];
+const carsMake = [            
+    { id: 0, carMakeName: "Bayerische Motoren Werke AG", carMakeAbrv: "BMW"},
+    { id: 1, carMakeName: "Volkswagen", carMakeAbrv: "VW"},
+    { id: 2, carMakeName: "Toyota Motor Corporation", carMakeAbrv: "Toyota"},
+    { id: 3, carMakeName: "Rimac Automobili", carMakeAbrv: "Rimac"},
+    { id: 4, carMakeName: "General Motors Company", carMakeAbrv: "GM"},
+    { id: 5, carMakeName: "Fiat Chrysler Automobiles N.V.", carMakeAbrv: "FCA"},
+];
+
+const carsModel = [
+    {id: 0, carMake: 0, carModelName: "Z4", carModelFuel: "Petrol", carModelInfo: "There are no more limits to driving pleasure in the BMW Z4 Roadster. A roadster that could not be any better: open, sporty and thrilling. With powerful driving dynamics and progressive design, it has only one aim: freedom between the road and sky."},
+    {id: 1, carMake: 0, carModelName: "i3", carModelFuel: "Electric", carModelInfo: "Get in and experience the future at your fingertips. The BMW i3 is full of innovative technologies it can use to confidently master your day-to-day life. It is always ready for the road and to explore new avenues. With style and a trend-setting design for electrifying driving pleasure."},
+    {id: 2, carMake: 0, carModelName: "530e", carModelFuel: "Hybrid ",carModelInfo: "A model athlete in the discipline of sustainability. The BMW 5 Series Sedan plug-in hybrid inspires thanks to the combination of powerful petrol engine, high-voltage battery and an electric motor from BMW eDrive technology that offers an impressive performance while simultaneously lowering fuel consumption and reducing CO2 emissions."},
+    {id: 3, carMake: 0, carModelName: "X1", carModelFuel: "Diesel", carModelInfo: "Milestones ahead: the BMW X1 has come to set standards. Its eagerness for action is shown above all in its athletic design. At first glance, it is an X model through and through: its exterior is characterised by short overhangs and a long wheelbase typical of a SAV (Sports Activity Vehicle). It makes its ambitions clear with convincing dynamics and agility."},
+    {id: 4, carMake: 3, carModelName: "Concept_Two", carModelFuel: "Electric", carModelInfo: "The Rimac C_Two: a pure electric GT hypercar as capable on track as it is crossing continents. A car as bespoke as it is user-friendly. A new breed of hypercar."},
+    {id: 5, carMake: 1, carModelName: "Arteon Shooting Brake", carModelFuel: "Hybrid", carModelInfo: "The Arteon Shooting brake offers extra space with an impressive 595-litre boot and enough legroom to keep your rear passengers comfortable. Also available as a plug-in hybrid, the Arteon Shooting Brake comes with a wide variety of tech features and gives you an incredibly smooth drive, particularly on the motorway."},
+    {id: 6, carMake: 1, carModelName: "Eos", carModelFuel: "Petrol", carModelInfo: "The Volkswagen Eos is a sport compact cabriolet coupé produced by the German automaker Volkswagen from 2006 to 2015. Assembled at AutoEuropa in Portugal, it was a convertible only compact coupé introduced as the successor of the Volkswagen Golf Cabriolet."},
+    {id: 7, carMake: 2, carModelName: "Corolla Hybrid", carModelFuel: "Hybrid", carModelInfo: "Awarded Green Car Journal's 2020 Green Car of the Year, Corolla Hybrid even comes with an enhanced Hybrid Battery Warranty that lasts for 10 years from date of first use, or 150,000 miles, whichever comes first."},
+    {id: 8, carMake: 2, carModelName: "Prius Prime", carModelFuel: "Hybrid", carModelInfo: "Placeholder"},
+    {id: 9, carMake: 2, carModelName: "Avalon", carModelFuel: "Hybrid", carModelInfo: "Placeholder"},
+    {id: 10, carMake: 4, carModelName: "Chevrolet Sequel", carModelFuel: "Hydrogen fuel cell", carModelInfo: "The Chevrolet Sequel was a purpose-built hydrogen fuel cell-powered concept car and sport utility vehicle from Chevrolet, employing the then latest generation of General Motors' fuel cell technology"},
+    {id: 11, carMake: 4, carModelName: "Chevrolet Bolt", carModelFuel: "Electric", carModelInfo: "The Chevrolet Bolt or Chevrolet Bolt EV is a front-motor, five-door all-electric small hatchback marketed by Chevrolet; developed and manufactured in partnership with LG Corporation"},
+    {id: 12, carMake: 5, carModelName: "124 Spider", carModelFuel: "Petrol", carModelInfo: "Exactly 50 years later, the iconic 1960s design is back in a roadster created for pure driving pleasure: the Fiat 124 Spider."},
+    {id: 13, carMake: 5, carModelName: "500", carModelFuel: "Electric", carModelInfo: "The all New 500 will be the first small car to bring Level 2 Autonomous Driving, which means a superior level of safety in the urban mobility. Your car will be your trusted co-driver: it will accelerate, brake and keep the lane autonomously."},
+    ];
