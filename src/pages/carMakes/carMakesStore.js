1363f5f6b6a84de402cce159a20139024a40b8bd
Oliver <oliversulyok@gmail.com>
2021-05-17 13:59:55 +0200

    Services branch created

--- src/pages/carMakes/carMakesStore.js
+++ src/pages/carMakes/carMakesStore.js
@@ -9,8 +9,6 @@ import React from 'react';
 class CarMakesStore {
     constructor(){
         makeObservable(this, {
-            carsMake: observable,
-            carsMakeHead: observable,
             lastId: observable,
             newCarMakeName: observable,
             newCarMakeAbrv: observable,
@@ -22,16 +20,6 @@ class CarMakesStore {
                 })
 
     }
-
-    carsMakeHead = ["Car Make Name", "Car Make Abbreviation"];
-    carsMake = [            
-        { id: 0, carMakeName: "Bayerische Motoren Werke AG", carMakeAbrv: "BMW"},
-        { id: 1, carMakeName: "Volkswagen", carMakeAbrv: "VW"},
-        { id: 2, carMakeName: "Toyota Motor Corporation", carMakeAbrv: "Toyota"},
-        { id: 3, carMakeName: "Rimac Automobili", carMakeAbrv: "Rimac"},
-        { id: 4, carMakeName: "General Motors Company", carMakeAbrv: "GM"},
-        { id: 5, carMakeName: "Fiat Chrysler Automobiles N.V.", carMakeAbrv: "FCA"},
-    ];
 
     lastId = this.carsMake.slice(-1)[0].id;
     newCarMakeName = React.createRef();
